4d69be333e2c956bbb00b026cf42b05ab4ff9515
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-11-30 14:01:58 -0600

    Create auto.js

---
+++ auto.js
@@ -0,0 +1,15 @@
+var mandrill = require('node-mandrill')('<your API Key>'); 
+
+function sendEmail ( _name, _email, _subject, _message) {
+    mandrill('/messages/send', {
+        message: {
+            to: [{email: _email , name: _name}],
+            from_email: 'noreply@yourdomain.com',
+            subject: _subject,
+            text: _message
+        }
+    }, function(error, response){
+        if (error) console.log( error );
+        else console.log(response);
+    });
+}
